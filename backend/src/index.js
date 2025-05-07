efe1646cff0000ac1a143a59d83d2255f235526e
Parag Deka <96935811+ParagSenDeka@users.noreply.github.com>
2025-05-08 00:15:49 +0530

    Update

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,28 +1,23 @@
 import express from "express";
-import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
 import cors from "cors";
-
 import path from "path";
-
+import dotenv from "dotenv"; // move this up
 import { connectDB } from "./lib/db.js";
-
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
-dotenv.config();
-
+const port = process.env.MY_PORT || 5001;
 const __dirname = path.resolve();
+dotenv.config({ path: path.resolve(path.join(__dirname,"../"),".env")});
 
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ limit: "50mb", extended: true }));
-
 app.use(cookieParser());
 app.use(
   cors({
     origin: "http://localhost:5173",
-
     credentials: true,
   })
 );
@@ -38,7 +33,11 @@ if (process.env.NODE_ENV === "production") {
   });
 }
 
-server.listen(process.env.PORT, () => {
-  console.log("server is running on PORT:" + process.env.PORT);
+server.listen(port, () => {
+  console.log("server is running on PORT:" + port);
   connectDB();
 });
+
+
+
+console.log(__dirname+"../");
